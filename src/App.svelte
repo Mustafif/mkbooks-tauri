816a4fac56227a7accbb773ac45184d7a5b83948
Mustafif <mustafif0929@gmail.com>
2022-01-24 20:39:38 -0500

    Update

---
+++ src/App.svelte
@@ -0,0 +1,61 @@
+<script>
+	
+</script>
+
+<main>
+	<header class="w3-container w3-padding-32 w3-center w3-black" id="home">
+        <h1 class="w3-jumbo"><span class="w3-hide-small">MKProject Books</span></h1>
+
+    <!-- <img src="Banner.png" alt="boy" class="w3-image" width="*" height="0.25*"> -->
+    </header>
+  <div class="w3-responsive w3-container w3-display-center w3-black" style="text-align: center;">
+	<a href="/book/Kotlin-Basics/"><img src="cover/KtBasics.png" class="center" alt="Kotlin"></a>
+	<a href="/book/CPP-Basics/"><img src="/cover/CPPBasics.png" class="center" alt="C++"></a>
+	<a href="book/Git-Basics/"><img src="/cover/GitBasics.png" class="center" alt="Git"></a>
+	<a href="book/Rust-Basics/"><img src="/cover/RsBasics.png" class="center" alt="Rust"></a>
+</div>    <!-- Footer -->
+    <footer class="w3-content w3-padding-64 w3-text-grey w3-xlarge">
+        <p class="w3-medium">Built by  <a href="https://github.com/MKProj/" target="_blank" class="w3-hover-text-green">MKProjects</a></p>
+        <!-- End footer -->
+    </footer>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #a45ee5;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+	.center {
+    display: inline;
+    margin-top: auto;
+    margin-bottom: auto;
+    margin-left: auto;
+    margin-right: auto;
+    width: 300px;
+  }
+  a {
+  text-decoration: none;
+  display: inline-block;
+  padding: 16px 16px;
+}
+
+a:hover {
+  background-color: #ddd;
+  color: black;
+}
+</style>
